60ccee2bb4f97620d3d4acb0751f26dd097026b2
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2022-05-09 03:09:42 +0300

    up

---
+++ co.js
@@ -0,0 +1,181 @@
+
+$("#user").attr("required", true);
+
+url = "https://ar.wikipedia.org/w/index.php?title=";
+
+const months = [
+    "يناير",
+    "فبراير",
+    "مارس",
+    "أبريل",
+    "مايو",
+    "يونيو",
+    "يوليو",
+    "أغسطس",
+    "سبتمبر",
+    "أكتوبر",
+    "نوفمبر",
+    "ديسمبر",
+];
+
+function get(user,limit,rccontinue,before) {
+    // $("#username").text(user);
+    // make user url
+    //-------------------------------------
+    if (rccontinue != '') {
+        //-----------------------
+        $(".newst").html("<a href='" + 'co.php?user=' + user + '&limit=' + limit + "'>الأحدث</a>");
+        //-----------------------
+        var ure = 'co.php?user=' + user + '&limit=' + limit;
+        //-----------------------
+        if (before != '') {
+            var ure = 'co.php?user=' + user + '&rccontinue=' + before + '&limit=' + limit;
+        };
+        //-----------------------
+        
+        $(".pref1").html("<a href='" + ure + "'>أحدث " + limit + "</a>");
+        //-----------------------
+    };
+    //-------------------------------------
+    var urs = 'co.php?user=' + user;
+    if (rccontinue != '' ) urs += '&rccontinue=' + rccontinue;
+    urs += '&limit=';
+
+    $(".limit20").html("<a href='" + urs + "20'>20</a>");
+    $(".limit50").html("<a href='" + urs + "50'>50</a>");
+    $(".limit100").html("<a href='" + urs + "100'>100</a>");
+    $(".limit250").html("<a href='" + urs + "250'>250</a>");
+    $(".limit500").html("<a href='" + urs + "500'>500</a>");
+    //-------------------------------------
+    $("#username").html("مساهمات <a href='https://ar.wikipedia.org/wiki/user:" + user + "' target='_blank'>مستخدم:" + user + "</a>");
+
+    var url = "https://ar.wikipedia.org/w/api.php"; 
+    var params = {
+        action: "query",
+        list: "recentchanges",
+        rcprop: "title|timestamp|ids|tags|sizes|sha1|redirect|patrolled|parsedcomment|loginfo|oresscores|flags|comment|userid|user",
+        rclimit: limit,
+        format: "json",
+        utf8 : 1,
+        rcuser : user,
+        rctype : "edit|new"
+    };
+    //--------------------
+    if (rccontinue != '') {
+        params.rccontinue = rccontinue;
+    };
+    //--------------------
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+    fetch(url)
+        .then(function(response){return response.json();})
+        .then(function(response) {
+            //-----------------------
+            var recentchanges = response.query.recentchanges;
+            //-----------------------
+            var numb = 1;
+            //-----------------------
+            for (var rc in recentchanges) {
+                	//---------
+                    const d = new Date(recentchanges[rc].timestamp);
+                	//---------
+                    var month = months[d.getMonth()];
+                	//---------
+                    var min = d.getMinutes();
+                    if (min.toString().length == 1) min = "0" + min;
+                    //----------------
+                    var hou = d.getHours();
+                    if (hou.toString().length == 1) hou = "0" + hou;
+                    //----------------------
+                    var day = d.getDate();
+                    if (day.toString().length == 1) day = "0" + day;
+                    //----------------------
+                    var date = min + ":&rlm;" + hou + "، " + day + " " + month + " " + d.getFullYear();
+                	//---------
+                    var space = "&nbsp;";
+                    var line = "";
+                    if (numb < 10) line += "&nbsp;";
+                    //line = "<span>" + recentchanges[rc].timestamp + "</span>&rlm;" + space;
+                    //--------------------------
+                    line += "<span id='edittype' style='display:none;'>" + recentchanges[rc].type + "</span>";
+                    //--------------------------
+                    line += "<span class='date' id='date'>" + date + "</span>";
+
+                    var dh = "<a href='" + url + recentchanges[rc].title + "&diff=prev&oldid=" + recentchanges[rc].revid + "'>فرق</a>";
+                    dh += space + "-" + space + "<a href='" + url + recentchanges[rc].title + "&action=history'>تاريخ</a>";
+
+                    line += "\n<span class='changeslist'> . . </span>( " + dh + " )<span class='changeslist'> . . </span>&rlm;\n";
+                    //-----------------------
+                    // diffrent between old lenth and new lenth
+                    var lenthdiff = recentchanges[rc].newlen - recentchanges[rc].oldlen;
+                    var diff = '';
+                    if (lenthdiff > 0) {
+                        lenthdiff = "+" + lenthdiff;
+                        diff = "<span class='diff' style='color:green;' id='diff" + numb + "'>" + lenthdiff + "</span>";
+                    } else {
+                        diff = "<span class='diff' style='color:red;' id='diff" + numb + "'>" + lenthdiff + "</span>";
+                    }
+                    
+                    line += '(' + diff +  ')<span class="changeslist"> . . </span>';
+                    //-----------------------
+                    if (recentchanges[rc].type == "edit") {
+                        line += "";
+                    } else {
+                        line += '<abbr style="text-underline-position: under;font-weight: bold;" title="أنشأ هذا التعديل صفحة جديدة">ج&zwnj;</abbr>';
+                    };
+                    
+                    //-----------------------
+                    var len = lenthdiff.toString().length;
+                    // alert(len);
+                    var digits = 8 - len ;
+                    // line += '(' + digits +  ')';
+                    //-----------------------
+                    var sas = "";
+                    for (var i = 0; i < digits; i++) {
+                        sas += "&nbsp;";
+                    };
+                    
+                    line += sas + '&rlm;\n';
+                    //-----------------------
+                    line += "\n<a href='" + url + recentchanges[rc].title + "'>" + recentchanges[rc].title + "</a>\n";
+                    //-----------------------
+                    var colimit = 200;
+                    var comment = recentchanges[rc].comment;
+                    if (comment.indexOf("/*") == -1) {
+                        comment = recentchanges[rc].parsedcomment;
+                        colimit = 500;
+                    };
+
+                    // strip long comments to 70 
+                    if (comment.length > colimit) {
+                        comment = comment.substring(0, colimit) + "...";
+                    };
+                    //-----------------------
+                    line += "\n<span id='comment'>(" + comment + ")</span>&rlm;\n";
+
+                    var newli = "\n<li>" + line + "</li>\n";
+                    tableBody = $("#rc");
+                    tableBody.append(newli);
+                    //---------
+                    numb++;               
+            }
+            //-----------------------"continue": { "rccontinue": "20220412203333|268179715",
+            var rcc = response.continue.rccontinue;
+            var h2 = '';
+            //-----------------------
+            if (rcc != undefined && rcc != null) {
+                // $("#rccontinue").text(rcc);
+                // alert(rcc);
+                var ure = 'co.php?user=' + user + '&rccontinue=' + rcc + '&before=' + rccontinue + '&limit=' + limit;
+                //---------------------------------
+                $(".next1").html("<a href='" + ure + "'>أقدم " + limit + "</a>");               
+                //---------------------------------
+            };
+            //-----------------------
+            //---------------------------------
+            //-----------------------
+        })
+        .catch(function(error){console.log(error);});
+
+};
---
+++ co.php
@@ -0,0 +1,132 @@
+<!DOCTYPE HTML>
+<html dir=rtl>
+<head>
+    <title>Usercontribs</title>
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+<?php
+    //---------------- 
+    $new = '
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    ';
+    //---------------- 
+    if ( $_SERVER['SERVER_NAME'] == 'localhost' ) { 
+        echo $new;
+    } else {
+        echo ' 
+    <script src="https://tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css">
+    ';
+    };
+?>
+    <link rel="stylesheet" type="text/css" href="https://tools-static.wmflabs.org/cdnjs/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+<style>
+    .boldfont {
+        font-weight: bold;
+    }
+    .spaces11 {
+        content: "&nbsp;";
+    }
+    .changeslist {
+        content: '. .';
+    }
+</style>
+</head>
+<body>
+<!-- <span id="rccontinue" style="display:none;"></span> -->
+<header class="app-header">
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<span class="navbar-brand" style="color:blue;"> 
+                    <a href="co.php">Usercontribs</a>
+                </span>
+			</div>
+			<ul class="nav navbar-nav">
+				<li class=""><a href="https://github.com/MrIbrahem/Usercontribs" target="_blank"><span style="font-size:16px">Github</span></a></li>
+			</ul>
+		</div>
+	</nav>
+</header>
+<div class="container">
+    <div class="panel panel-info">
+        <div class="panel-heading">
+                <form id='form' action='co.php' method='get'>
+                    <input type='text' name='user' id='user' placeholder='اسم المستخدم' required>
+                    <input type='number' name='limit' id='limit' value='250'>
+                    <!-- <input type='submit' value='بدء' onclick='test()'> -->
+                    <input type='submit' value='بدء'>
+                </form>
+                <p><span id="msg" style='color:red;'></span></p>
+        </div>
+    </div>
+  <div class="panel panel-default">
+    <div class="panel">
+        <div class="row">
+            <div class="col-sm-6"> 
+                <input id="myInput" type="text" placeholder="فرز">
+            </div>
+            <div class="col-sm-6"> 
+                <h2><span id="username">اسم المستخدم</span></h2>
+            </div>
+        </div>
+        
+        <div>
+            (<span class="boldfont newst">الأحدث</span> | الأقدم)
+            عرض (<span class="boldfont pref1">أحدث <span class="nowlimit"></span></span>  | <span class="boldfont next1">أقدم <span class="nowlimit"></span></span>)&nbsp;( <span class="boldfont limit20">20</span> | <span class="boldfont limit50">50</span> | <span class="boldfont limit100">100</span> | <span class="boldfont limit250">250</span> | <span class="boldfont limit500">500</span> ).
+            
+        </div>
+    </div>
+    <div id="panel-body" class="panel-body">
+        <ol id="rc"></ol>
+    </div>
+    <div class="panel">
+        <div>
+        (<span class="boldfont newst">الأحدث</span> | الأقدم)
+            عرض (<span class="boldfont pref1">أحدث <span class="nowlimit"></span></span>  | <span class="boldfont next1">أقدم <span class="nowlimit"></span></span>)&nbsp;( <span class="boldfont limit20">20</span> | <span class="boldfont limit50">50</span> | <span class="boldfont limit100">100</span> | <span class="boldfont limit250">250</span> | <span class="boldfont limit500">500</span> ).
+            
+        </div>
+    </div>
+  </div>
+
+</div>
+<script>
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#rc li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+</script>
+<script src="co.js"></script>
+
+<?php
+    $limit = $_REQUEST["limit"] ? $_REQUEST["limit"] : '250';
+
+    $rccontinue = $_REQUEST["rccontinue"] ? $_REQUEST["rccontinue"] : '';
+    $user = $_REQUEST["user"] ? $_REQUEST["user"] : 'Mr.Ibrahembot';
+    $before = $_REQUEST["before"] ? $_REQUEST["before"] : '';
+
+    print("
+    <script>
+    $('#limit').val('$limit');
+    $('.nowlimit').text('$limit');
+    $('#user').val('$user');
+    get('$user','$limit','$rccontinue','$before');
+    </script>
+    ");
+
+?>
+</div>
+
+</body>
+
+</html>
